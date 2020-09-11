f986d96f92bbcf348cb3cfdffd945e8758a6c22e
DonMaks97 <71144710+DonMaks97@users.noreply.github.com>
2020-09-11 13:35:19 -0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+#
+book, variable_2, a = "biblia de los caidos", 200, "casa"
+print(variable_2, book)
+print(book + a)
+
+book_name = "libro" #snake case
+bookName = "libro" #camel case
+BookName = "libro 3" #Pascal case
+#el mas usado es camel case.
+print ("Ingresa un numero")
+var_1 = int(5)
+var_2 = int(input()) 
+while var_1 < var_2:
+    print (var_2+10)
